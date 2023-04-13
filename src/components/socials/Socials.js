626d658cf6d31e14ce9e16da3e13ea67033eac5a
ekeneakubue <ekeneakubue@gmail.com>
2023-04-13 15:52:24 -0700

    update social icons

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -7,7 +7,7 @@ export default function Socials() {
   return (
     <div className='container-fluid text-light social-wrap'>      
       <div className='row justify-content-center'>        
-        <a href="https://wa.me/+2348032744865" target="_blank" rel="noopener npreferrer"><BsWhatsapp style={{ marginBottom: "2px" }} className='text-light social bg-info'/></a>
+        <a href="https://wa.me/+2348032744865" target="_blank" rel="noopener npreferrer"><BsWhatsapp style={{ marginBottom: "2px" }} className='text-light social'/></a>
         <a href="https://github.com/ekeneakubue" target="_blank"><BsGithub style={{ marginBottom: "2px" }} className='text-light social'/></a>
         <a href="https://www.linkedin.com/in/ekene-akubue-58046b77/" target="_blank"><BsLinkedin style={{ marginBottom: "2px" }} className='text-light social'/></a>
         <a href="https://www.instagram.com/ekeneakubue/" target="_blank"><BsInstagram style={{ marginBottom: "2px" }} className='text-light social'/></a>
